8e23d9d06b2c3c510b5c6e863e7fecd69b2e993e
Matthew D. Buehler <mbuehle3@gmail.com>
2021-11-27 12:01:18 -0600

    adding in script to create plots

---
+++ output-media/slim-graphics.R
@@ -0,0 +1,17 @@
+setwd("../output/")
+getwd()
+
+output <- read.csv("results-20-10.csv", header = TRUE)
+output
+
+head(output)
+
+par(mfrow=c(3,1))
+
+plot(mean.phenotype~gen,data = output, type = "l")
+abline (h = 10, col = "red")
+plot(hetero.~gen, data = output, type = "l")
+plot (pop.size~gen,data = output, type = "l")
+
+o
+?plot()
