f7927cb5e3cb3cfba46e6edacf02f7db6420ed7f
Matthew D. Buehler <mbuehle3@gmail.com>
2021-11-28 15:23:06 -0600

    Wrapping up

--- output-media/slim-graphics.R
+++ output-media/slim-graphics.R
@@ -17,6 +17,18 @@ for (i in 0:reps) {
 p1.files.0
 p1.files.1
 
+for (i in 0:reps) {
+        # i = 1
+        # nam <- paste("p1.files.",i, sep = "")
+        # nam
+        
+        assign(paste("p3.files.",i, sep = ""), list.files(path = paste(dir,"/",i, sep = ""), pattern = paste(c("*-p3.results.csv"))))
+}
+p1.files.0
+p1.files.1
+
+
+
 p1.files.0[1] == p1.files.1[1]
 
 p1.files.1
@@ -112,7 +124,7 @@ for (file in 1:length(p1.files.1)) {
     # p1.data
     jpeg(paste("../output-media/",p1.data.root,".jpg",sep = ""))
     par(mfrow=c(3,1))
-    plot(mean.phenotype~gen, data = rep0.data , type = "l", cex = 3.5, col = "blue", ylim = c(-15,15))
+    plot(mean.phenotype~gen, data = rep0.data , type = "l", cex = 3.5, col = "blue", ylim = c(-20,20))
     lines(mean.phenotype~gen, data = rep1.data, lwd= 1.5, col = "red")
     abline (h = 10, col = "red", cex = 2.5)
     title(p1.data.root, cex = 5)
@@ -123,6 +135,36 @@ for (file in 1:length(p1.files.1)) {
     
     dev.off()
 }
+
+
+for (file in 1:length(p3.files.1)) {
+    print(file)
+    rep.0 = p3.files.0[file]
+    rep.1 = p3.files.1[file]
+    print(rep.0)
+    print(rep.1)
+    rep0.data <- read.csv(paste(dir,"/0/",rep.0,sep = ""), header = TRUE)
+    rep1.data <- read.csv(paste(dir,"/1/",rep.1,sep = ""), header = TRUE)
+    p3.data.root <- tools::file_path_sans_ext(rep.0)
+    p3.data.root
+    # p1.data.root
+    # p1.data
+    jpeg(paste("../output-media/",p3.data.root,".jpg",sep = ""))
+    par(mfrow=c(3,1))
+    plot(mean.phenotype~gen, data = rep0.data , type = "l", cex = 3.5, col = "blue", ylim = c(-20,20))
+    lines(mean.phenotype~gen, data = rep1.data, lwd= 1.5, col = "red")
+    abline (h = 10, col = "red", cex = 2.5)
+    title(p1.data.root, cex = 5)
+    plot(hetero~gen, data = rep0.data, type = "l", cex = 1.5, col = "green")
+    lines(hetero~gen, data = rep1.data, lwd= 1.5, col = "red")
+    plot(pop.size~gen,data = rep0.data, type = "l", cex = 1.5, col = "purple")
+    lines(pop.size~gen, data = rep1.data, lwd= 1.5, col = "red")
+    
+    dev.off()
+}
+
+
+
 
 for (p3.file in p3.files){
     print(p3.file)
